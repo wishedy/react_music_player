1642974a18a104125753b99886af83e40564d50d
张恒 <zhfirefly@yahoo.com>
2018-07-03 20:34:00 +0800

    问题：创建react播放器 描述：搭建webpack环境，并运行正常 author:zhangheng

--- app/components/SwitchBar.js
+++ app/components/SwitchBar.js
@@ -3,15 +3,19 @@ import './SwitchBar.scss';
 class SwitchBar extends Component{
     constructor(props){
         super(props);
+        //this.changeIndex = this.changeIndex.bind(this,dir);
+    }
+    changeIndex(dir){
+        console.log(dir);
     }
     render(){
         return(
             <section className='reactMusicPlayerSwitchBar'>
-                <section className='reactMusicPlayerChange upIcon'></section>
+                <section className='reactMusicPlayerChange upIcon' onClick={this.changeIndex.bind(this,0)}></section>
                 <section className='reactMusicPlayerChange timeCount'>{this.props.PlayTime}</section>
                 <section className='reactMusicPlayerChange split'>/</section>
                 <section className='reactMusicPlayerChange timeCount'>{this.props.Duration}</section>
-                <section className='reactMusicPlayerChange downIcon'></section>
+                <section className='reactMusicPlayerChange downIcon' onClick={this.changeIndex.bind(this,1)}></section>
             </section>
         )
     }
